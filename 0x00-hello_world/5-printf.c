76fabaa4691f151ef9674887ef315dbbf951fbdc
miqrtey <99749840+miqrtey@users.noreply.github.com>
2022-03-13 14:40:57 +0400

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+es (10 sloc)  206 Bytes
+   
+#include <stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
