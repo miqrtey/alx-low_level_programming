8a16e6e10309775b2a3944f3cd935606d126b419
miqrtey <99749840+miqrtey@users.noreply.github.com>
2022-03-17 11:12:55 +0400

    ego

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,21 @@
-#include "main.h"
+#include "_putchar.h"
+
+/**
+ * main - program that prints _putchar.
+ *
+ * Return: int
+ */
 
 int main(void)
 {
 	int n = 0;
-	char str_mn[9] = "main";
+	char str_hbt[9] = "_putchar";
 
 	while (n < 9)
 	{
-
-		_putchar(str_mn[n]);
+		_putchar(str_hbt[n]);
 		n += 1;
 	}
 	_putchar('\n');
 	return (0);
+i}
